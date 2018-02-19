05bb4787c1b818e3f67bd9818d63427547db1bd2
kibizu <ma.pomponi@libero.it>
2018-02-19 19:30:02 +0100

    Java sets global variables already at a default of 0 so you can simplify your code to: int scoreTeamA; FiXED What actually occurs is that the state of the app gets destroyed. Fixed

--- app/src/main/java/com/example/android/footballscorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/footballscorekeeper/MainActivity.java
@@ -7,29 +7,127 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+
     /**
      * theese are the global variables for the A Team
      */
-    int scoreTeamA = 0;
-    int touchDownTeamA = 0;
-    int fieldGoalTeamA = 0;
-    int safetyTeamA = 0;
-    int dropTeamA = 0;
+    private static final String SCORE_TEAM_A = "scoreTeamA";
+    private int scoreTeamA;
+    private TextView scoreTeamAView;
+
+    private static final String TOUCHDOWN_TEAM_A = "touchDownTeamA";
+    private int touchDownTeamA;
+    private TextView touchDownTeamAView;
+
+    private static final String FIELD_GOAL_TEAM_A = "fieldGoalTeamA";
+    private int fieldGoalTeamA;
+    private TextView fieldGoalTeamAView;
+
+    private static final String SAFETY_TEAM_A = "safetyTeamA";
+    private int safetyTeamA;
+    private TextView safetyTeamAView;
+
+    private static final String DROP_TEAM_A = "dropTeamA";
+    private int dropTeamA;
+    private TextView dropTeamAView;
 
     /**
      * theese are the global variables for the B Team
      */
+    private static final String SCORE_TEAM_B = "scoreTeamB";
+    private int scoreTeamB;
+    private TextView scoreTeamBView;
 
-    int scoreTeamB = 0;
-    int touchDownTeamB = 0;
-    int fieldGoalTeamB = 0;
-    int safetyTeamB = 0;
-    int dropTeamB = 0;
+    private static final String TOUCHDOWN_TEAM_B = "touchDownTeamB";
+    private int touchDownTeamB;
+    private TextView touchDownTeamBView;
+
+    private static final String FIELD_GOAL_TEAM_B = "fieldGoalTeamB";
+    private int fieldGoalTeamB;
+    private TextView fieldGoalTeamBView;
+
+    private static final String SAFETY_TEAM_B = "safetyTeamB";
+    private int safetyTeamB;
+    private TextView safetyTeamBView;
+
+    private static final String DROP_TEAM_B = "dropTeamB";
+    private int dropTeamB;
+    private TextView dropTeamBView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // If we have a saved state then we can restore it now
+        if (savedInstanceState != null) {
+
+            scoreTeamA = savedInstanceState.getInt(SCORE_TEAM_A, 0);
+            touchDownTeamA = savedInstanceState.getInt(TOUCHDOWN_TEAM_A,0);
+            fieldGoalTeamA = savedInstanceState.getInt(FIELD_GOAL_TEAM_A,0);
+            safetyTeamA = savedInstanceState.getInt(SAFETY_TEAM_A,0);
+            dropTeamA = savedInstanceState.getInt(DROP_TEAM_A,0);
+
+            scoreTeamB = savedInstanceState.getInt(SCORE_TEAM_B, 0);
+            touchDownTeamB = savedInstanceState.getInt(TOUCHDOWN_TEAM_B,0);
+            fieldGoalTeamB = savedInstanceState.getInt(FIELD_GOAL_TEAM_B,0);
+            safetyTeamB = savedInstanceState.getInt(SAFETY_TEAM_B,0);
+            dropTeamB = savedInstanceState.getInt(DROP_TEAM_B,0);
+
+
+        }
+
+        scoreTeamAView = (TextView) findViewById(R.id.score_a_team);
+        scoreTeamAView.setText(Integer.toString(scoreTeamA));
+
+        touchDownTeamAView = (TextView) findViewById(R.id.touchDown_a_team);
+        touchDownTeamAView.setText(Integer.toString(touchDownTeamA));
+
+        fieldGoalTeamAView = (TextView) findViewById(R.id.fieldGoal_a_team);
+        fieldGoalTeamAView.setText(Integer.toString(fieldGoalTeamA));
+
+        safetyTeamAView = (TextView) findViewById(R.id.safety_a_team);
+        safetyTeamAView.setText(Integer.toString(safetyTeamA));
+
+        dropTeamAView = (TextView) findViewById(R.id.drop_a_team);
+        dropTeamAView.setText(Integer.toString(dropTeamA));
+
+
+        scoreTeamBView = (TextView) findViewById(R.id.score_b_team);
+        scoreTeamBView.setText(Integer.toString(scoreTeamB));
+
+        touchDownTeamBView = (TextView) findViewById(R.id.touchDown_b_team);
+        touchDownTeamBView.setText(Integer.toString(touchDownTeamB));
+
+        fieldGoalTeamBView = (TextView) findViewById(R.id.fieldGoal_b_team);
+        fieldGoalTeamBView.setText(Integer.toString(fieldGoalTeamB));
+
+        safetyTeamBView = (TextView) findViewById(R.id.safety_b_team);
+        safetyTeamBView.setText(Integer.toString(safetyTeamB));
+
+        dropTeamBView = (TextView) findViewById(R.id.drop_b_team);
+        dropTeamBView.setText(Integer.toString(dropTeamB));
+
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(SCORE_TEAM_A, scoreTeamA);
+        outState.putInt(TOUCHDOWN_TEAM_A, touchDownTeamA);
+        outState.putInt(FIELD_GOAL_TEAM_A, fieldGoalTeamA);
+        outState.putInt(SAFETY_TEAM_A, safetyTeamA);
+        outState.putInt(DROP_TEAM_A, dropTeamA);
+
+        outState.putInt(SCORE_TEAM_B, scoreTeamB);
+        outState.putInt(TOUCHDOWN_TEAM_B, touchDownTeamB);
+        outState.putInt(FIELD_GOAL_TEAM_B, fieldGoalTeamB);
+        outState.putInt(SAFETY_TEAM_B, safetyTeamB);
+        outState.putInt(DROP_TEAM_B, dropTeamB);
+
     }
 
     /**
